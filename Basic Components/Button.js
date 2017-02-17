c4655d344c8135019385a6df18e3faac070c8871
kylwil29 <kylwil29@ps553095.dreamhostps.com>
2017-02-17 11:10:54 -0800

    Created Button and Modal JS

---
+++ Basic Components/Button.js
@@ -0,0 +1,42 @@
+function Button(buttonName){
+	this.init(buttonName);
+	this.element;
+	this.value;
+	this.type;
+}
+
+/*Extend Component*/
+Button.prototype = Component.prototype;
+
+//Function: set value in button
+//Parameter: value -> the value for the button
+Button.prototype.setVaule = function(value){
+	this.value = value;
+}
+
+//Function: set the type of the button
+//Parameter: type -> the type for the button (default:clickable)
+Button.prototype.setType = function(type){
+	if(type === undefined){
+		this.type = 'button';
+	}else{
+		this.type = type;
+	}
+}
+
+//Function: Create HTML element
+//Parameter: None
+Button.prototype.view = function(){
+	var button = document.createElement("BUTTON");
+	button.setAttribute('component-id',this.name);
+	button.setAttribute('type',this.type);
+	button.setAttribute('value',this.value);
+	this.element = button;
+	return button;
+};
+
+//Function: function to execute when the button is clicked
+//Parameter: callback -> the function that will be executed
+Button.prototype.onClick = function(callback){
+	callback();
+};
---
+++ Basic Components/Modal.js
@@ -0,0 +1,34 @@
+
+function Modal(modalName){
+	this.init(modalName);
+	this.element;
+}
+
+/*Extending the Component Object*/
+Modal.prototype = Component.prototype;
+
+//Function: Create HTML element
+//Parameter: None
+//Return: the element that was created
+Modal.prototype.view = function(){
+	var modal = document.createElement("DIV");
+	modal.setAttribute('component-id',this.name);
+	modal.setAttribute('style','position:absolute;z-index:1;width:100%;height:100%;background:rgba(0,0,0,0.5);');
+	this.element = modal;
+	return modal;
+};
+
+//Function: hide the modal
+//Parameter: None
+Modal.prototype.hide = function(){
+	this.element.style.display = 'none';
+};
+
+//Function: show the modal
+//Parameter: None
+Modal.prototype.show = function(){
+	this.element.style.display = 'block';
+};
+
+
+
